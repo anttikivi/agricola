694d914a750a9eb6a5f10a9e33218947abab4999
Antti Kivi <antti@anttikivi.fi>
2024-11-25 22:48:07 +0200

    feat: add a panic handler

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,5 +1,9 @@
 package cmd
 
+import "github.com/anttikivi/agricola/internal/logging"
+
 func Execute() int {
+	defer logging.PanicHandler()
+
 	return 0
 }
---
+++ internal/logging/panic.go
@@ -0,0 +1,69 @@
+package logging
+
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+	"sync"
+)
+
+// The panic handler is based on the panic handler in OpenTofu.
+// See: https://github.com/opentofu/opentofu/blob/main/internal/logging/panic.go
+
+// This output is shown if a panic happens.
+const panicOutput = `
+!!!!!!!!!!!!!!!!!!!!!!!!!!! AGRICOLA CRASH !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+Agricola crashed! This is always indicative of a bug within Agricola.
+Please report the crash with Agricola[1] so that we can fix this.
+
+When reporting bugs, please include your Agricola version, the stack trace
+shown below, and any additional information which may help replicate the issue.
+
+[1]: https://github.com/anttikivi/agricola/issues
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!! AGRICOLA CRASH !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+`
+
+// In case multiple goroutines panic concurrently, ensure only the first one
+// recovered by PanicHandler starts printing.
+var panicMutex sync.Mutex
+
+// PanicHandler is called to recover from an internal panic in Agricola, and
+// augments the standard stack trace with a more user friendly error message.
+// PanicHandler must be called as a deferred function, and must be the first
+// defer called at the start of a new goroutine.
+func PanicHandler() {
+	// Have all managed goroutines checkin here, and prevent them from exiting
+	// if there's a panic in progress. While this can't lock the entire runtime
+	// to block progress, we can prevent some cases where Agricola may return
+	// early before the panic has been printed out.
+	panicMutex.Lock()
+	defer panicMutex.Unlock()
+
+	recovered := recover()
+	panicHandler(recovered, nil)
+}
+
+func panicHandler(recovered interface{}, trace []byte) {
+	if recovered == nil {
+		return
+	}
+
+	fmt.Fprint(os.Stderr, panicOutput)
+	fmt.Fprint(os.Stderr, recovered, "\n")
+
+	// When called from a deferred function, debug.PrintStack will include the
+	// full stack from the point of the pending panic.
+	debug.PrintStack()
+	if trace != nil {
+		fmt.Fprint(os.Stderr, "With go-routine called from:\n")
+		os.Stderr.Write(trace)
+	}
+
+	// An exit code of 11 keeps us out of the way of the detailed exitcodes
+	// from plan, and also happens to be the same code as SIGSEGV which is
+	// roughly the same type of condition that causes most panics.
+	os.Exit(11)
+}
