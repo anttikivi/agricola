6d41c6ef6855dbbe3153f45fc244526c6a17a3ca
Antti Kivi <antti@anttikivi.fi>
2024-11-26 11:20:43 +0200

    feat: add `cmd` package that runs the program

---
+++ cmd/cmd.go
@@ -0,0 +1,6 @@
+package cmd
+
+// Execute runs the program and returns its exit code.
+func Execute() int {
+	return 0
+}
--- main.go
+++ main.go
@@ -1,4 +1,11 @@
 package main
 
+import (
+	"os"
+
+	"github.com/anttikivi/agricola/cmd"
+)
+
 func main() {
+	os.Exit(cmd.Execute())
 }
