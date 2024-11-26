4909414f44fa351a54c9251342e3b809d48d9247
Antti Kivi <antti@anttikivi.fi>
2024-11-26 23:58:31 +0200

    feat!: remove the `Help` function of the command interface before it is needed

--- internal/command/command.go
+++ internal/command/command.go
@@ -7,9 +7,6 @@ const (
 
 // A Command represents a command line subcommand.
 type Command interface {
-	// Help returns a long help string for the command.
-	Help() string
-
 	// Execute executes the command with the given arguments and returns the exit code.
 	Execute(args []string) int
 
